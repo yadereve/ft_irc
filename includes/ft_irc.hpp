c8f493594baa024851f11df977017239d5730e5b
Yaroslav <wikifixcom@gmail.com>
2025-02-27 20:36:24 +0000

    add class Ft_irc

---
+++ includes/ft_irc.hpp
@@ -0,0 +1,16 @@
+#ifndef FT_IRC_HPP
+#define FT_IRC_HPP
+
+class Ft_irc
+{
+public:
+	Ft_irc();
+	Ft_irc(const Ft_irc& other);
+	Ft_irc& operator=(const Ft_irc& other);
+	~Ft_irc();
+
+private:
+
+};
+
+#endif
